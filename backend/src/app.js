1a2316ceb3788497e8fa886fb75fc3fc6aaeb2ff
brunobcpro <brunobcpro@gmail.com>
2023-08-30 14:37:02 -0300

    adição json e analise de cadastro
    Arquivo Bruno.json criado e adição do modulo fs para leitura de json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,36 +1,58 @@
-const express = require('express')
-const app = express()
-const exphbs  = require('express-handlebars');
-var handle = exphbs.create({
-  defaultLayout: 'main'
-  });
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
-  
-const port = 3000
+const fs = require('fs'); 
 
-//Config
-    //Template engine
-    app.engine('handlebars', handle.engine);
-    app.set('view engine', 'handlebars');
-    
-    // Body parser
-    app.use(bodyParser.urlencoded({extended: false}))
-    app.use(bodyParser.json())
+const port = 3000;
 
-//Rotas
+// Config
+const handle = exphbs.create({
+  defaultLayout: 'main'
+});
 
-  app.get('/login', function(req, res){
-    res.render('login')
-  })
+// Template engine
+app.engine('handlebars', handle.engine);
+app.set('view engine', 'handlebars');
 
-  app.post('/add',function(req,res){
-    if(req.body.login === "Bruno"){
-      res.send("Login correto")
-    }else{
-      res.send("Login errado")
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Rotas
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.post('/add', function (req, res) {
+  const username = req.body.login;
+  const password = req.body.senha;
+
+  // Lê o arquivo bruno.json
+  fs.readFile('bruno.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erro ao ler o arquivo JSON:', err);
+      res.redirect('/login');
+      return;
     }
-  })
+
+    try {
+      const jsonData = JSON.parse(data);
+
+      // Verifica as credenciais
+      if (jsonData.name === username && jsonData.senha === password) {
+        res.send("Olá,"+username + "\n vc estuda na "+ jsonData.faculdade+ " e tem " + jsonData.idade + " anos");
+      } else {
+        res.redirect('/login');
+      }
+    } catch (parseError) {
+      console.error('Erro ao fazer parse do JSON:', parseError);
+      res.redirect('/login');
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
