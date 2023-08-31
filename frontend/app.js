cfd9a32fbaac60776748cde4956f7c13ad5baff7
brunobcpro <brunobcpro@gmail.com>
2023-08-31 17:06:16 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,21 @@
-let axios = require("axios")
+const axios = require("axios")
+const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+
+// Config
+const handle = exphbs.create({
+    defaultLayout: 'main'
+  });
+  
+  // Template engine
+  app.engine('handlebars', handle.engine);
+  app.set('view engine', 'handlebars');
+  
+  // Body parser
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  
+
 
 axios.get('http://localhost:3000/')
     .then(response => console.log(response))
